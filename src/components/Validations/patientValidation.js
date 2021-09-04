dba41efa03a04e97d81cea52ddf79abadf2fe272
Hamza960 <shaikhhamzasaleem960@gmail.com>
2021-09-04 12:23:24 +0530

    validations

---
+++ src/components/Validations/patientValidation.js
@@ -0,0 +1,27 @@
+import * as yup from 'yup';
+
+
+export const validationSchema = yup.object().shape({
+        patient_name: yup.string().required("Patient name is required"),
+        patient_email: yup.string().email().required("Patient email is required"),
+        patient_referred_by: yup.string().required("Please enter a name"),
+        patient_mobile: yup.number()./* matches( "Must be only digits") */min(10, "Mobile number should not be more than 10 digits").max(10, "Mobile number should not be less than 10 digits").required().positive().integer(),
+        patient_age: yup.string().required("Patient age is required"),
+        patient_gender: yup.string().required("Patient gender is required"),
+        patient_nationality: yup.string().required("Patient nationality is required"),
+        patient_document: yup.string().required("Patient document is required"),
+        patient_reports: yup.string().required("Patient reports are required"),
+        current_diagnosis: yup.string().required("Patient current diagnosis is required"),
+        insurance_card_copy: yup.string().required("Patient insurance card copy is required"),
+        medical_history: yup.string().required("Patient medical history is required"),
+        proposed_treatment_plan: yup.string().required("Patient treatment plan is required"),
+        transport_support_needed: yup.string().required("Please select an option"),
+        languages_spoken: yup.string().required("Please select an option"),
+        food_preferences: yup.string().required("Food preferences is required"),
+        accomodation: yup.string().required("Accomodation is required"),
+        preferred_hospital_visit: yup.string().required("Please select an option"),
+        proposal_date: yup.string().required("Please select proposal date"),
+        from_date: yup.string().required("Please select a date"),
+        to_date: yup.string().required("Please select a date")
+
+})
---
+++ src/components/Validations/quoteValidation.js
@@ -0,0 +1,27 @@
+import * as yup from 'yup';
+
+
+export const quoteSchema = yup.object().shape({
+        select_doctor: yup.string().required("Please select a doctor"),
+        select_anesthesiologist: yup.string().email().required("Please select anesthediologist"),
+        treatment_plan: yup.string().required("Treatment plan is required"),
+        estimate_price: yup.string().required("Estimate price is required"),
+        inclusion: yup.string().required("Inclusion is required"),
+        exclusion: yup.string().required("Exclusion is required"),
+        expected_length: yup.string().required("Expected length is required"),
+        estimate_copay: yup.string().required("Estimated copay is required"),
+        type_of_anesthesia: yup.string().required("Please select an option"),
+        type_of_room: yup.string().required("Type of room is required"),
+        free_room_upgrade: yup.string().required("Please select an option"),
+        free_physiotherapy: yup.string().required("Please select an option"),
+        free_other_speciality_consultant: yup.string().required("Please select an option"),
+        free_telephonic_feedback: yup.string().required("Please select an option"),
+        free_annual_checkup: yup.string().required("Please select an option"),
+        pickup_and_drop: yup.string().required("Please select an option"),
+        free_patient_dedicated_relationship: yup.string().required("This field is required"),
+        benefits_for_patient: yup.string().required("Please enter a response"),
+        benefits_for_attendent: yup.string().required("Please enter e response"),
+        food_menu: yup.string().required("Food menu is required"),
+        confirmation: yup.string().required("Confirmation is required"),
+        general_disclaimer: yup.string().required("General disclaimer is required"),
+})
