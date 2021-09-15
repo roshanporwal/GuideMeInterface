a7a26385c01016d846b5365ef49a3e7838ea0f31
Hamza960 <shaikhhamzasaleem960@gmail.com>
2021-09-15 18:27:09 +0530

    navbars

--- src/Navbar/admin_navbar.js
+++ src/Navbar/admin_navbar.js
@@ -2,6 +2,13 @@ import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import 'font-awesome/css/font-awesome.min.css';
 import {  Link,useHistory } from 'react-router-dom';
+import './style.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHospital } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faPaperclip } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function ADMIN_NAVBAR(props) {
   const history = useHistory();
@@ -18,23 +25,52 @@ export default function ADMIN_NAVBAR(props) {
 
     return(
         <>
-<Navbar bg="light" expand="lg">
+ <Navbar bg="light" expand="lg">
   <Container>
     <Navbar.Text style = {{fontSize: "22px"}}>GuideMeDoc</Navbar.Text>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav style = {{paddingLeft: 200}}>
-      <i class="far fa-hospital"></i>
-      <Nav.Link className = "text-dark" as={Link} to= '/admin/dashboard'>Dashboard</Nav.Link>
-        <Nav.Link  className = "text-dark" as={Link} to= '/admin/hospital/profile'>Hospitals profile</Nav.Link>
-        <Nav.Link  className = "text-dark" >Smart Search</Nav.Link>
-        <Nav.Link  className = "text-dark"  >MIS</Nav.Link>
-        <Nav.Link  className = "text-dark" onClick={logout}>Logout</Nav.Link>
+      <FontAwesomeIcon icon={faUserAlt} style = {{fontSize: 22, marginLeft: "20px"}} />
+      <Nav.Link className = "text-dark" as={Link} to= '/admin/dashboard'><p style = {{color:"black"}}>Patients</p></Nav.Link>
+      <FontAwesomeIcon icon={faHospital} style = {{fontSize: 22, marginLeft: "20px"}} />
+        <Nav.Link  className = "text-dark" as={Link} to= '/admin/hospital/profile'><p style = {{color:"black"}}>Hospitals</p></Nav.Link>
+        <FontAwesomeIcon icon={faSearch} style = {{fontSize: 22, marginLeft: "20px"}} />
+        <Nav.Link  className = "text-dark" ><p style = {{color:"black"}}>Smart Search</p></Nav.Link>
+        <FontAwesomeIcon icon={faPaperclip} style = {{fontSize: 22, marginLeft: "20px"}} />
+        <Nav.Link  className = "text-dark"  ><p style = {{color:"black"}}>MIS</p></Nav.Link>
+        <Nav.Link  className = "text-dark" onClick={logout}><p style = {{color: "black", marginLeft: "1rem"}}><FontAwesomeIcon icon={faSignOutAlt}  style ={{fontSize: 22, marginLeft: "20rem"}} /></p></Nav.Link> 
        
       </Nav>
     </Navbar.Collapse>
   </Container>
 </Navbar>
-        </>
+     
+ {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand">GuideMeDoc</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" as={Link} to= '/admin/dashboard'>Dashboard <span class="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" as={Link} to= '/admin/hospital/profile'>Hospital Profile</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" >Smart Search</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" >MIS</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" onClick={logout}>Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav> 
+ */}    </>
     )
 }
--- src/Navbar/hospital_navbar.js
+++ src/Navbar/hospital_navbar.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import 'font-awesome/css/font-awesome.min.css';
 import {  Link,useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './style.css';
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function HospitalNavbar(props) {
   const history = useHistory();
@@ -19,22 +24,57 @@ export default function HospitalNavbar(props) {
 
     return(
         <>
-<Navbar bg="light" expand="lg">
+ <Navbar bg="light" expand="lg">
   <Container>
-    <Navbar.Text><img src="assets\images\GuideMeDoc.png" alt="" /></Navbar.Text>
+    <Navbar.Text>GuideMeDoc</Navbar.Text>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav style = {{paddingLeft: 200}}>
-      <i className = "fa fa-hospital-alt"></i><Nav.Link className = "text-dark" as={Link} to= '/hospital/dashboard'>Dashboard</Nav.Link>
-        <Nav.Link  className = "text-dark"as={Link} to= '/admin/hospital/profile'>Profile</Nav.Link>
-        <Nav.Link  className = "text-dark"as={Link} to= '/hospital/doctors'>Doctors</Nav.Link>
-        <Nav.Link  className = "text-dark"as={Link} to= '/admin'>admin</Nav.Link>
-        <Nav.Link  className = "text-dark" onClick={logout}>Logout</Nav.Link>
+      
+      <Nav.Link className = "text-dark"  as={Link} to= '/hospital/dashboard'><p style = {{color: "black", marginLeft: "20rem"}}>Dashboard</p></Nav.Link>
+      {/* <FontAwesomeIcon icon={faUsers} style = {{fontSize: 22, marginLeft: "20px"}} />*/}
+        
+        
+        <Nav.Link  className = "text-dark"as={Link} to= '/hospital/doctors'><p style = {{color: "black", marginLeft: "2rem"}}>Doctors</p></Nav.Link>
+        {/* <FontAwesomeIcon icon={faUser}  style ={{fontSize: 22, marginLeft: "20px"}} />
+        <Nav.Link  className = "text-dark"as={Link} to= '/admin'><p style = {{color: "black", marginLeft: "5px"}}>Admin</p></Nav.Link>
+        <FontAwesomeIcon icon={faSignOutAlt}  style ={{fontSize: 22, marginLeft: "20px"}} />*/}
+        <Nav.Link  className = "text-dark"as={Link} to= '/admin/hospital/profile'><p style = {{color: "black", paddingLeft: "35rem"}}><FontAwesomeIcon icon={faUser} style = {{fontSize: 22, marginLeft: "20px"}} /></p></Nav.Link> 
+        <Nav.Link  className = "text-dark" onClick={logout}><p style = {{color: "black", marginLeft: "1rem"}}><FontAwesomeIcon icon={faSignOutAlt}  style ={{fontSize: 22, marginLeft: "20px"}} /></p></Nav.Link> 
        
       </Nav>
     </Navbar.Collapse>
   </Container>
-</Navbar>
-        </>
+</Navbar> 
+{/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" >GuideMeDoc</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active d-flex">
+        <i className = "fa fa-columns" style = {{fontSize: 22, marginTop: 10}}></i>
+        <a style = {{color: "black"}} className="nav-link"as={Link} to= '/hospital/dashboard'>Dashboard <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item d-flex">
+      <i className = "fa fa-users" style = {{fontSize: 22, marginTop: 10}}></i>
+        <a className="nav-link" as={Link} to= '/admin/hospital/profile'>Profile</a>
+      </li>
+      <li className="nav-item d-flex">
+      <i className = "fa fa-stethoscope" style = {{fontSize: 22, marginTop: 10}}></i>
+        <a className="nav-link" as={Link} to= '/hospital/doctors'><p style = {{color: "black"}}>Doctors</p></a>
+      </li> 
+      <li className="nav-item d-flex" style = {{color: "black"}}>
+      <i className = "fa fa-user" style = {{fontSize: 22, marginTop: 10}}></i>
+        <a className="nav-link" as={Link} to= '/admin'>Admin</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" onClick={logout}>Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+ */}        </>
     )
 }
