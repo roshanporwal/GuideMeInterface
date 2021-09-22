c4d53c1954b662bac41299483187991537e70751
nyakole <akolenikhil@gmail.com>
2021-09-22 23:52:13 +0530

    constant changes

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 const constants = {
     serverBaseUrl: 'http://proctologyalliance.com',
-    clientBaseUrl:`http://192.46.209.112:3000/`,
+    clientBaseUrl:`http://192.46.209.112/`,
    
   
   };
