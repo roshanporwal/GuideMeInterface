dd806b2f9626e9801256f100ffaffcba9c75ce7a
nyakole <akolenikhil@gmail.com>
2021-10-11 23:16:27 +0530

    changes by ajit sir

--- src/admin/admin_home.js
+++ src/admin/admin_home.js
@@ -36,39 +36,34 @@ function ADMIN_Home(props) {
 
 
     <Router>
-      <div className = "container-fluid">
-      <div className="content row" >
-        <div className="col-md-4">
-        <img className = "login_image" style = {{width: "300px", height: "200px"}} src="/assets/images/GuideMeDocLogo.png" alt=""  />
-            <img className = "login_image" style = {{marginTop: "-20px"}} src="assets\images\admin_login.jpg" alt=""  />
-
-         
-
-        </div>
-        <div className="col-md-8">
-          <h2 className="login_title">Welcome, Admin</h2>
-          <form className="login_form">
-            <div className="email_field">
-              <label htmlFor="exampleInputEmail1">Email</label>
-              <input style={{ borderRadius: "5px" }} className="form-control" type="text" name="login_id" value={values.login_id} onChange={handleChange} placeholder="name@domain.com" />
-            </div>
-            <div className="password_field">
-              <label htmlFor="exampleInputEmail1">Password</label>
-              <input style={{ borderRadius: "5px" }} className="form-control " type="password" name="password" value={values.password} onChange={handleChange} placeholder="**********" />
-            </div>
-            <div className="submit_btn">
-
-              <button className="join_button" onClick={handleSubmit} type="submit">Join Now</button>
-
-            </div>
-
-
-
-
-          </form>
-        </div>
-      </div>
-    </div>
+		<div className = "container-fluid">
+			<div className="content row" >
+				<div className="col-md-4 m-0 text-center">
+					<img className = "Logo" src="/assets/images/GuideMeDocLogo.png" alt=""  />
+				</div>
+			</div>	
+			<div className="row">
+				<div className="col-md-4">
+					<img className = "loginImage" src="assets\images\admin_login.jpg" alt=""  />
+				</div>
+				<div className="col-md-8">
+					<form className="login_form">
+						<h2 className="login_title">Welcome, Admin.</h2>
+						<div className="email_field">
+						  <label htmlFor="exampleInputEmail1">Email</label>
+						  <input style={{ borderRadius: "5px" }} className="form-control" type="text" name="login_id" value={values.login_id} onChange={handleChange} placeholder="name@domain.com" />
+						</div>
+						<div className="password_field">
+						  <label htmlFor="exampleInputEmail1">Password</label>
+						  <input style={{ borderRadius: "5px" }} className="form-control " type="password" name="password" value={values.password} onChange={handleChange} placeholder="**********" />
+						</div>
+						<div className="submit_btn">
+						  <button className="join_button" onClick={handleSubmit} type="submit">Join Now</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
     </Router>
   );
 }
