1f5bb0f4cad3df51a9e7a4129bee2b4ca6726ab6
Naseem khan <53051431+megamindnk@users.noreply.github.com>
2021-12-21 12:01:38 +0530

    branching added

--- src/Navbar/hospital_navbar.js
+++ src/Navbar/hospital_navbar.js
@@ -2,11 +2,12 @@ import React , { useState ,useEffect} from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 import {Link, useHistory } from 'react-router-dom';
 import './style.css';
+import { Dropdown } from 'react-bootstrap';
 
 export default function HospitalNavbar(props) {
   const history = useHistory();
   const [allhospital, setAllhospital] = useState([])
-
+  const [loggedname,setLoggedName] = useState('');
 
   useEffect(() => {
    
@@ -19,10 +20,17 @@ export default function HospitalNavbar(props) {
    if(data===null){
      return 
    }
+   let loggedin = localStorage.getItem("login")
+   if(loggedin===null){
+     return 
+   }
+  loggedin = JSON.parse(loggedin);
+  setLoggedName(loggedin.hospital_name); 
    const  islogin = JSON.parse(data)
    setAllhospital(islogin)
   }
   async function onSubmit(data) {
+    console.log(data)
     localStorage.setItem('login', JSON.stringify(data));
     window.location.reload();
   }
@@ -57,11 +65,31 @@ export default function HospitalNavbar(props) {
                             <li className="nav-item d-flex justify-content-center align-items-center">
                               <Link className = "nav-link" to = '/hospital/doctors'><span style = {{color: "#164473"}}>Doctors</span></Link>
                             </li>
+                            <li className="nav-item d-flex justify-content-center align-items-center">
+                            {allhospital.length ? (
+                                <>
+                                  <Dropdown>
+                                    <Dropdown.Toggle className='select-branch' id="dropdown-basic">
+                                      {loggedname}
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                      {allhospital.map((item,index) => {
+                                        return (
+                                          <Dropdown.Item href="#" onClick={() => onSubmit(item)} key={index}>{item.hospital_name}</Dropdown.Item>
+                                        )
+                                      })}
+                                    </Dropdown.Menu>
+                                  </Dropdown>
+                                </>
+
+                              ) : null }
+                          </li>
                           </ul>
                           <span className="navbar-text">
                             <Link className = "nav-link" to = '/hospital/profile'><span style = {{color: "#164473"}}>
                             <img src="/assets/images/icons/patients.png" alt=""/></span></Link> 
                           </span>
+                          
                           <span className="navbar-text">
                             <Link className = "nav-link" to ='#' onClick={logout}><span style = {{color: "#164473"}}>
                             <img src="/assets/images/icons/logout.png" alt=""/></span></Link>  
