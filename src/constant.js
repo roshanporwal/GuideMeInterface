5a7a38e7a893bf86245db6bf75be0bf0e4787891
nyakole <akolenikhil@gmail.com>
2022-01-18 23:00:01 +0530

    constant changes

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 const constants = {
-    serverBaseUrl: "http://192.46.209.112:8082/",//'http://localhost:8080',
+    serverBaseUrl: "http://192.46.209.112:8080/",//'http://localhost:8080',
     clientBaseUrl:/*'http://localhost:3000'*/`http://guidemedoc.com/`,
   };
 
