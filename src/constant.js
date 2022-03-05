bdae7f9e59ccc820e392c1ae27c7337cbd770493
nishant2106 <nishantpandey2106@gmail.com>
2022-03-05 23:14:46 +0530

    192.46.209.112

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 const constants = {
-    serverBaseUrl: "http://45.79.120.37:8080/",//'http://localhost:8082',
+    serverBaseUrl: "http://192.46.209.112:8080/",//'http://localhost:8082',
     clientBaseUrl:/*'http://localhost:3000'*/`http://guidemedoc.com/`,
   };
 
