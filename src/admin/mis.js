cbdba47f7e8a977119800ea7bc8bb805cd117607
nishant2106 <nishantpandey2106@gmail.com>
2022-03-23 14:29:19 +0530

    mis ui changed

--- src/admin/mis.js
+++ src/admin/mis.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import 'font-awesome/css/font-awesome.min.css';
+import React from "react";
+import "font-awesome/css/font-awesome.min.css";
 
-
-
-import './style.css'
+import "./style.css";
 import ADMIN_NAVBAR from "../Navbar/admin_navbar";
-import Button from '@restart/ui/esm/Button';
-import constants from '../constant';
-
-
+import Button from "@restart/ui/esm/Button";
+import constants from "../constant";
+import { FiDownload } from "react-icons/fi";
 function Mis() {
-   
-  
-    
-        return (
-            <>
-                <ADMIN_NAVBAR />
-                <Button onClick={() => { window.location.href =`${constants.serverBaseUrl}/uploadexcel/patient_excel` }}>patient</Button>
-                <Button onClick={()=> { window.location.href =`${constants.serverBaseUrl}/uploadexcel/hospital_excel` }}>Hospital</Button>
-            </>
-        );
-   
+    return (
+        <>
+            <ADMIN_NAVBAR />
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-6 text-center download-button">
+                        <Button
+                            className="btn btn-info button-style"
+                            onClick={() => {
+                                window.location.href = `${constants.serverBaseUrl}/uploadexcel/patient_excel`;
+                            }}
+                        >
+                            Download Patient Data &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FiDownload />
+                        </Button>
+                    </div>
+                    <div className="col-md-4 text-center download-button">
+                        <Button
+                            className="btn btn-info button-style"
+                            onClick={() => {
+                                window.location.href = `${constants.serverBaseUrl}/uploadexcel/hospital_excel`;
+                            }}
+                        >
+                            Download Hospital Data&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <FiDownload />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Mis;
