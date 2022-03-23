0be4276e337667ac1414a30981bb113ba8faeb56
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2022-03-23 14:10:30 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 const constants = {
   serverBaseUrl: "https://guidemedoc.com/api",//"http://45.79.120.37:8080",'http://localhost:8082',
-  clientBaseUrl:/*'http://localhost:3000'*/`http://guidemedoc.com/`,
+  clientBaseUrl:/*'http://localhost:3000'*/`https://guidemedoc.com/`,
 };
 
 export default  constants;
